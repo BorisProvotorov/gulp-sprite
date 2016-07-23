0a0bf04faf8102f199169059784e558bb6c929c3
Vincent Ballut <kaayru@gmail.com>
2016-07-23 18:47:54 +0200

    Adds comments to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,17 @@ function onError(error) {
 }
 
 gulp.task('sprite', function () {
-    var spriteData = gulp.src('source_img/*.png')
+    var spriteData = gulp.src('source_img/*.png') // Source images
         .pipe(spritesmith({
-            imgName: 'sprite.png',
-            imgPath: 'sprite.png',
-            cssName: 'sprite.css',
-            cssTemplate: 'sprite.mustache'
+            imgName: 'sprite.png', // Name of the final sprite
+            imgPath: 'sprite.png', // Path to the final sprite
+            cssName: 'sprite.css', // Name of the CSS file that will be generated
+            cssTemplate: 'sprite.mustache' // Path to the mustache template (used to build the CSS file)
         }));
 
     var imgStream = spriteData.img
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.')); // Path to the final sprite
 
     return cssStream = spriteData.css
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.')); // Path to the generated CSS file
 });
